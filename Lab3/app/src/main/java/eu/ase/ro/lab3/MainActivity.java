bb7dc30c023c23bf94319269ce6b30f3b1145fdc
Romeo <popescuromeo22@stud.ase.ro>
2024-10-17 11:03:05 +0300

    first commit

---
+++ Lab3/app/src/main/java/eu/ase/ro/lab3/MainActivity.java
@@ -0,0 +1,47 @@
+package eu.ase.ro.lab3;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fab = findViewById(R.id.fabAddMovie);
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.menu_option_1) {
+            Toast.makeText(getApplicationContext(),
+                    R.string.option1, Toast.LENGTH_SHORT).show();
+        }
+        if(item.getItemId() == R.id.menu_option_2) {
+            Toast.makeText(getApplicationContext(),
+                    R.string.option2, Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
+}
