8e934600431b85bc47277f4295a46ef1c53868a4
Romeo <popescuromeo22@stud.ase.ro>
2024-11-15 09:27:29 +0200

    added menu, finished seminar 1

--- SeminarFromZero/app/src/main/java/com/example/seminarfromzero/MainActivity.java
+++ SeminarFromZero/app/src/main/java/com/example/seminarfromzero/MainActivity.java
@@ -2,20 +2,29 @@ package com.example.seminarfromzero;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvMainActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("MainActivity", "onCreate");
+
+        tvMainActivity = findViewById(R.id.tvMainActivity);
+
     }
 
     @Override
@@ -24,4 +33,26 @@ public class MainActivity extends AppCompatActivity {
         Log.i("MainActivity", "onStart");
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("MainActivity", "onPause");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_option1) {
+            Toast.makeText(getApplicationContext(), R.string.menu_option1, Toast.LENGTH_LONG).show();
+        }
+        else if (item.getItemId() == R.id.menu_option2) {
+            Toast.makeText(getApplicationContext(), R.string.menu_option2, Toast.LENGTH_LONG).show();
+        }
+        return true;
+    }
 }
